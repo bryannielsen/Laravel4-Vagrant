7cf3999de7ec91b46c24cd310410cc8c6385d64b
Taylor Otwell <taylorotwell@gmail.com>
2012-05-10 22:50:27 -0500

    fluffing up the files.

--- app/app.php
+++ app/app.php
@@ -1,10 +1,32 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Detect The Application Environment
+|--------------------------------------------------------------------------
+|
+| Illuminate takes a dead simple approach to application environments.
+| Just specify the hosts that belong to a given environment, and we
+| will quickly detect and set the application environment for you.
+|
+*/
+
 $app->detectEnvironment(array(
 
-	'local' => array('localhost', 'illuminate.app'),
+	'local' => array('localhost', '*.dev'),
 
 ));
+
+/*
+|--------------------------------------------------------------------------
+| Load The Environment Bulb
+|--------------------------------------------------------------------------
+|
+| You may specify a "bulb" for each environment. The default bulb will be
+| included on every request and the environment bulb gives a chance to
+| customize the application such as tweaking the IoC bindings, etc.
+|
+*/
 
 require __DIR__.'/bulbs/default.php';
 
@@ -12,6 +34,17 @@ if (file_exists($bulb = __DIR__.'/bulbs/'.$app['env'].'.php'))
 {
 	require $bulb;
 }
+
+/*
+|--------------------------------------------------------------------------
+| Load The Application Routes
+|--------------------------------------------------------------------------
+|
+| The Application routes are kept separate from the application starting
+| just to keep the file a little cleaner. We'll go ahead and load in
+| all of the routes now and return the application to the caller.
+|
+*/
 
 require __DIR__.'/routes.php';
 
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,17 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for your application.
+| It's a breeze. Just tell Illuminate which URIs it should respond to
+| and give it a Closure to execute when that URI is accessed. It is
+| a walk in the park. Enjoy the simplicity of HTTP and routing.
+|
+*/
+
 $app->get('/', function() use ($app)
 {
 	return 'Hello World!';
