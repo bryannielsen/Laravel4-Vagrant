410236e5adc56c56efd2418af1fe3c85860538a2
Taylor Otwell <taylorotwell@gmail.com>
2012-05-16 22:18:00 -0500

    work on route file.

--- app/bulbs/production.php
+++ app/bulbs/production.php
@@ -15,69 +15,26 @@ $app['debug'] = false;
 
 /*
 |--------------------------------------------------------------------------
-| Register The Illuminate Filesystem Manager
+| Register The Base Service Provider
 |--------------------------------------------------------------------------
 |
-| The Illuminate file system provides a nice abstraction over the file
-| system, which makes testing code that gets or puts files on disk
-| much easier, since the file systems can easily be mocked out.
+| The Illuminate base service provider registers all of the core pieces
+| of the Illuminate framework including session, caching, encryption
+| and more. It's simply a convenient wrapper for the registration.
 |
 */
 
-$app['files'] = $app->share(function()
-{
-	return new Illuminate\Filesystem;
-});
+$app->register(new Illuminate\Foundation\BaseServiceProvider);
 
 /*
 |--------------------------------------------------------------------------
-| Register The Illuminate Event Dispatcher
+| Set The Application Encryption Key
 |--------------------------------------------------------------------------
 |
-| The Illuminate event dispatcher provides a simpler, yet powerful way
-| to build de-coupled systems using events, including queueing and
-| flushing events. We'll go ahead and register a shared object.
+| This key is used by the Illuminate encrypter service and should be set
+| to a random, long string, otherwise your encrypted values will not
+| be secure. Be sure to change this before deploying your website!
 |
 */
 
-$app['events'] = $app->share(function()
-{
-	return new Illuminate\Events\Dispatcher;
-});
-
-/*
-|--------------------------------------------------------------------------
-| Register The Illuminate Encrypter
-|--------------------------------------------------------------------------
-|
-| The Illuminate encrypter service provides a nice, convenient wrapper
-| around the mcrypt encryption library. By default, strong AES-256
-| encryption is provided out of the box. Just be sure to change
-| your application key, as strong encryption depends on it!
-|
-*/
-
-$app['encrypter.key'] = 'YourSecretKey';
-
-$app['encrypter'] = $app->share(function() use ($app)
-{
-	$key = $app['encrypter.key'];
-
-	return new Illuminate\Encrypter(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC, $key);
-});
-
-/*
-|--------------------------------------------------------------------------
-| Register The Illuminate Session
-|--------------------------------------------------------------------------
-|
-| The Illuminate session library provides a variety of simple, clean
-| session drivers for use by your application. By default, we'll
-| use the light Cookie driver for convenience and simplicity.
-|
-*/
-
-$app['session'] = $app->share(function() use ($app)
-{
-	return new Illuminate\Session\CookieStore($app['encrypter']);
-});
+$app['encrypter.key'] = 'YourSecretKey!';
--- app/routes.php
+++ app/routes.php
@@ -8,7 +8,23 @@
 | Here is where you can register all of the routes for your application.
 | It's a breeze. Just tell Illuminate which URIs it should respond to
 | and give it a Closure to execute when that URI is accessed. It is
-| a walk in the park. Enjoy the simplicity of HTTP and routing.
+| a walk in the park. Enjoy the simplicity of HTTP based routing.
+|
+*/
+
+$app->get('/', function() use ($app)
+{
+	return 'Hello World!';
+});
+
+/*
+|--------------------------------------------------------------------------
+| Application Events
+|--------------------------------------------------------------------------
+|
+| These events run before and after requests to your application and
+| are very useful for pre and post processing. By default they're
+| responsible for starting and finishing the session handling.
 |
 */
 
@@ -17,12 +33,8 @@ $app->before(function($request) use ($app)
 	$app['session']->start($request);
 });
 
+
 $app->after(function($request, $response) use ($app)
 {
 	$app['session']->finish($response);
-});
-
-$app->get('/', function() use ($app)
-{
-	return 'Hello World!';
 });
