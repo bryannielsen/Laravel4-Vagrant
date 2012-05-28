ff94d3f9e17e476053ce382e35abc97cbe9a6bd7
Taylor Otwell <taylorotwell@gmail.com>
2012-05-28 15:09:04 -0500

    work on translation.

--- app/bulbs/local.php
+++ app/bulbs/local.php
@@ -2,21 +2,17 @@
 
 return array(
 
-	'app' => array(
+	/*
+	|--------------------------------------------------------------------------
+	| Application Debug Mode
+	|--------------------------------------------------------------------------
+	|
+	| When your application is in debug mode, detailed error messages with
+	| stack traces will be shown on every error that occurs within your
+	| application. When disabled, a simple, generic error is shown.
+	|
+	*/
 
-		/*
-		|--------------------------------------------------------------------------
-		| Application Debug Mode
-		|--------------------------------------------------------------------------
-		|
-		| When your application is in debug mode, detailed error messages with
-		| stack traces will be shown on every error that occurs within your
-		| application. When disabled, a simple, generic error is shown.
-		|
-		*/
-
-		'debug' => true,
-
-	),
+	'debug' => true,
 
 );
--- app/routes.php
+++ app/routes.php
@@ -14,5 +14,19 @@
 
 $app->get('/', function() use ($app)
 {
+	/*
+	$app['locale'] = 'en';
+	$app->register(new Silex\Provider\TranslationServiceProvider, array(
+		'locale' => 'en',
+		'locale_fallback' => 'en',
+		'translator.domains' => array(
+			'messages' => array(
+				'en' => array(
+					'hello' => 'Hello, :name',
+				),
+			),
+		),
+	));
+	*/
 	return $app->twig->render('hello.twig.html');
 });
