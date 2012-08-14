be30b8613778f8ebce27ba96ebcaffbc119e0e95
Taylor Otwell <taylorotwell@gmail.com>
2012-08-13 23:18:05 -0500

    work on config.

--- app/config/app.php
+++ app/config/app.php
@@ -21,8 +21,8 @@ return array(
 	|--------------------------------------------------------------------------
 	|
 	| The application locale determines the default locale that will be used
-	| by the translation service provider. You're free to set this value
-	| to any of the locales which are supported by your applications.
+	| by the translation service provider. You are free to set this value
+	| to any of the locales which will be supported by the application.
 	|
 	*/
 
