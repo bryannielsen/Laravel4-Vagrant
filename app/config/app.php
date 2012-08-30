f867d1e0b528752632a396399a9d8cfe961e792d
Taylor Otwell <taylorotwell@gmail.com>
2012-08-30 08:09:59 -0500

    Added Hashing provider.

--- app/config/app.php
+++ app/config/app.php
@@ -64,6 +64,7 @@ return array(
 		'Illuminate\Foundation\Providers\EncrypterServiceProvider',
 		'Illuminate\Foundation\Providers\EventsServiceProvider',
 		'Illuminate\Foundation\Providers\FilesystemServiceProvider',
+		'Illuminate\Foundation\Providers\HashServiceProvider',
 		'Illuminate\Foundation\Providers\SessionServiceProvider',
 		'Illuminate\Foundation\Providers\ValidatorServiceProvider',
 
