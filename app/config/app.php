7b2741031457e89c08c6598994a65fff4a1152e9
Taylor Otwell <taylorotwell@gmail.com>
2012-10-22 22:56:57 -0500

    flesh out mail config file.

--- app/config/app.php
+++ app/config/app.php
@@ -103,6 +103,7 @@ return array(
 		'Illuminate\Foundation\Providers\FilesystemServiceProvider',
 		'Illuminate\Foundation\Providers\HashServiceProvider',
 		'Illuminate\Foundation\Providers\LogServiceProvider',
+		'Illuminate\Foundation\Providers\MailServiceProvider',
 		'Illuminate\Foundation\Providers\MigrationServiceProvider',
 		'Illuminate\Foundation\Providers\PaginationServiceProvider',
 		'Illuminate\Foundation\Providers\RedisServiceProvider',
