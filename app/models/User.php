d16753c3d74e7599692603e22d82ff67a11ae0ee
Taylor Otwell <taylorotwell@gmail.com>
2012-10-26 22:08:45 -0500

    Renamed "domain" to "models" and added default User model.

---
+++ app/models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Auth\EloquentUser;
+
+class User extends EloquentUser {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'users';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = array('password');
+
+}
