b922e6a1c85f63c4ee15d42bfd2e44a16471ad70
Taylor Otwell <taylorotwell@gmail.com>
2012-10-26 20:46:31 -0500

    Adding SeedServiceProvider to config - also added "seeds" directory.

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array(
 		'Illuminate\Foundation\Providers\MigrationServiceProvider',
 		'Illuminate\Foundation\Providers\PaginationServiceProvider',
 		'Illuminate\Foundation\Providers\RedisServiceProvider',
+		'Illuminate\Foundation\Providers\SeedServiceProvider',
 		'Illuminate\Foundation\Providers\SessionServiceProvider',
 		'Illuminate\Foundation\Providers\TranslationServiceProvider',
 		'Illuminate\Foundation\Providers\ValidatorServiceProvider',
