a147339fbe6314e434b9eac132e6ff42acc95d7b
Taylor Otwell <taylorotwell@gmail.com>
2012-10-28 21:51:51 -0500

    Make a filters file by default.

--- app/routes.php
+++ app/routes.php
@@ -15,25 +15,3 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
-
-/*
-|--------------------------------------------------------------------------
-| Application & Route Filters
-|--------------------------------------------------------------------------
-|
-| Below you will find the "before" and "after" events for the application
-| which may be used to do any work before or after a request into your
-| application. Here you may also register your custom route filters.
-|
-*/
-
-App::before(function($request)
-{
-	// Fired before all requests to the application...
-});
-
-
-App::after(function($request, $response)
-{
-	// Fired after all requests to the application...
-});
--- app/start/production.php
+++ app/start/production.php
@@ -30,3 +30,16 @@ App::error(function(Exception $exception, $code)
 {
 	Log::error($exception);
 });
+
+/*
+|--------------------------------------------------------------------------
+| Require The Filters File
+|--------------------------------------------------------------------------
+|
+| Next we will load the filters file for the application. This gives us
+| a nice separate location to store our route and application filter
+| definitions instead of putting them all in the main routes file.
+|
+*/
+
+require __DIR__.'/../filters.php';
