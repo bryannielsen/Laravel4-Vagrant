9339d29dcc4eb6feef19e9709fe509f1cf35ab43
Taylor Otwell <taylorotwell@gmail.com>
2012-11-18 14:20:26 -0600

    added collation to database options.

--- app/config/database.php
+++ app/config/database.php
@@ -53,13 +53,14 @@ return array(
 		),
 
 		'mysql' => array(
-			'driver'   => 'mysql',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'charset'  => 'utf8',
-			'prefix'   => '',
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'database',
+			'username'  => 'root',
+			'password'  => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
 		),
 
 		'pgsql' => array(
