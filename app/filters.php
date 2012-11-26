eb5f447471120d2c7cdfb4324022307853bfeee6
Taylor Otwell <taylorotwell@gmail.com>
2012-11-25 20:19:00 -0600

    move basic filters into filter file.

--- app/filters.php
+++ app/filters.php
@@ -21,3 +21,44 @@ App::after(function($request, $response)
 {
 	//
 });
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Filters
+|--------------------------------------------------------------------------
+|
+| The following filters are used to verify that the user of the current
+| session is logged into this application. Also, a "guest" filter is
+| responsible for performing the opposite. Both provide redirects.
+|
+*/
+
+Route::filter('auth', function()
+{
+	if (Auth::guest()) return Redirect::route('login');
+});
+
+
+Route::filter('guest', function()
+{
+	if (Auth::check()) return Redirect::to('/');
+});
+
+/*
+|--------------------------------------------------------------------------
+| CSRF Protection Filter
+|--------------------------------------------------------------------------
+|
+| The CSRF filter is responsible for protecting your application against
+| cross-site request forgery attacks. If this special token in a user
+| session does not match the one given in this request, we'll bail.
+|
+*/
+
+Route::filter('csrf', function()
+{
+	if (Session::getToken() != Input::get('csrf_token'))
+	{
+		throw new Illuminate\Session\TokenMismatchException;
+	}
+});
