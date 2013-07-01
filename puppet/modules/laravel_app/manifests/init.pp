be88fc8b9b8c5705bc0d09a79ef0f931ed362405
Bryan Nielsen <bryan@packettide.com>
2013-06-30 20:00:09 -0400

    use composer for laravel install closes #18

--- puppet/modules/laravel_app/manifests/init.pp
+++ puppet/modules/laravel_app/manifests/init.pp
@@ -8,35 +8,35 @@ class laravel_app
    	exec { 'install composer':
 	    command => 'curl -s https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin',
 	    require => Package['php5-cli'],
+	    unless => "[ -f /usr/local/bin/composer ]"
 	}
 
 	exec { 'global composer':
 		command => "sudo mv /usr/local/bin/composer.phar /usr/local/bin/composer",
 		require => Exec['install composer'],
+		unless => "[ -f /usr/local/bin/composer ]"
 	}
 
-	# exec { 'get laravel packages':
-	# 	command => "composer install'",
-	# 	cwd => '/var/www/',
-	# 	require => [Exec['global composer'], Package['git-core']],
-	# 	timeout => 900,
-	# }
-
-	exec { 'get laravel packages':
-		command => "/bin/sh -c 'cd /var/www/ && composer install'",
-		require => [Exec['global composer'], Package['git-core']],
-		creates => "/var/www/composer.lock",
-		timeout => 900,
+	# Check to see if there's a composer.json and app directory before we delete everything
+	# We need to clean the directory in case a .DS_STORE file or other junk pops up before
+	# the composer create-project is called
+	exec { 'clean www directory': 
+		command => "/bin/sh -c 'cd /var/www && find -mindepth 1 -delete'",
+		unless => [ "test -f /var/www/composer.json", "test -d /var/www/app" ]
 	}
 
-	exec { 'remove optimized class loader':
-		command => "/bin/sh -c 'rm /var/www/bootstrap/compiled.php'",
-		onlyif => "[ -f /var/www/bootstrap/compiled.php ]"
+
+	exec { 'create laravel project':
+		command => "/bin/sh -c 'cd /var/www/ && composer create-project laravel/laravel . -n --prefer-dist'",
+		require => [Exec['global composer'], Package['git-core'], Exec['clean www directory']],
+		creates => "/var/www/composer.json",
+		timeout => 1800,
 	}
 
 	exec { 'get laravel updates':
         command => "/bin/sh -c 'cd /var/www/ && composer update'",
-        require => [Exec['get laravel packages'], Exec['remove optimized class loader'], Package['git-core']],
+        require => Package['git-core'],
+        onlyif => "[ -f /var/www/composer.json ]",
         timeout => 900,
 	}
 
