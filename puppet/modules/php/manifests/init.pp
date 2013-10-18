c1fad6b101ecf784aee4d89b0f4c8e38af113c74
Bryan Nielsen <bryan@packettide.com>
2013-10-17 20:26:13 -0400

    Add a php.ini file for CLI, fixes #35

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -35,11 +35,21 @@ class php
     { 
         "/etc/php5/apache2/php.ini":
             ensure  => present,
-            owner => root, group => root,
+            owner   => root, group => root,
             notify  => Service['apache2'],
             #source  => "/vagrant/puppet/templates/php.ini",
             content => template('php/php.ini.erb'),
             require => [Package['php5'], Package['apache2']],
+    }
+
+    file 
+    { 
+        "/etc/php5/cli/php.ini":
+            ensure  => present,
+            owner   => root, group => root,
+            notify  => Service['apache2'],
+            content => template('php/cli.php.ini.erb'),
+            require => [Package['php5']],
     }
     
 }
